b18deb22c669878caea29638be0d66d2537bc88a
Viktor Trako <viktor.trako@btinternet.com>
2012-11-10 12:53:17 +0000

    Translate to ar-drone commands so that we can start to use them

--- config/config.js
+++ config/config.js
@@ -1,13 +1,13 @@
 module.exports = {
   1  : "land",
-  2  : "forward",
+  2  : "front",
   3  : "emergency",
   4  : "left",
   5  : "takeoff",
   6  : "right",
   7  : "clockwise",
   8  : "back",
-  9  : "anticlockwise",
+  9  : "counterclockwise",
   0  : "flip",
   '*': "up",
   '#': 'down'
